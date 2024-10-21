534522b3fffec3b218ecb7c0d3d14a9852d0ac79
arnav <arnabdas2753@gmail.com>
2024-10-21 19:14:39 +0530

    fix some

--- src/app/components/ui/navbar-menu.tsx
+++ src/app/components/ui/navbar-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import Image from "next/image";
 import Img from "@/app/public/Img.png"
 
@@ -110,7 +110,7 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+export const HoveredLink = ({ children, ...rest }: React.AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps) => {
   return (
     <Link
       {...rest}
